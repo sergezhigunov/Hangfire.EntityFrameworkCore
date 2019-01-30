80429a6b9b5ee1cd9da02373d3fb658bbbc169df
Sergey Zhigunov <serge.zhigunov@outlook.com>
2019-01-31 03:53:42 +0500

    Implement job storage

---
+++ src/Hangfire.EntityFrameworkCore/EntityFrameworkCoreJobStorage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Hangfire.Storage;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hangfire.EntityFrameworkCore
+{
+    internal class EntityFrameworkCoreJobStorage : JobStorage
+    {
+        private readonly DbContextOptions<HangfireContext> _options;
+
+        public EntityFrameworkCoreJobStorage(
+            DbContextOptions<HangfireContext> options)
+        {
+            _options = options ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        public override IStorageConnection GetConnection()
+        {
+            return new EntityFrameworkCoreJobStorageConnection(_options);
+        }
+
+        public override IMonitoringApi GetMonitoringApi()
+        {
+            return new EntityFrameworkCoreJobStorageMonitoringApi(_options);
+        }
+    }
+}
---
+++ tests/Hangfire.EntityFrameworkCore.Tests/EntityFrameworkCoreJobStorageFacts.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Hangfire.EntityFrameworkCore.Tests
+{
+    public class EntityFrameworkCoreJobStorageFacts : HangfireContextTest
+    {
+        [Fact]
+        public void Ctor_Throws_IfOptionsParameterIsNull()
+        {
+            DbContextOptions<HangfireContext> options = null;
+
+            Assert.Throws<ArgumentNullException>(nameof(options),
+                () => new EntityFrameworkCoreJobStorage(options));
+        }
+
+        [Fact]
+        public void Ctor_CreatesInstance()
+        {
+            var options = new DbContextOptions<HangfireContext>();
+
+            var instance = new EntityFrameworkCoreJobStorage(options);
+
+            Assert.Same(options,
+                Assert.IsType<DbContextOptions<HangfireContext>>(
+                    instance.GetFieldValue("_options")));
+        }
+
+        [Fact]
+        public void GetConnection_ReturnsCorrectResult()
+        {
+            var options = new DbContextOptions<HangfireContext>();
+            var instance = new EntityFrameworkCoreJobStorage(options);
+
+            var result = instance.GetConnection();
+
+            Assert.NotNull(result);
+            var connection = Assert.IsType<EntityFrameworkCoreJobStorageConnection>(result);
+
+            Assert.Same(options,
+                Assert.IsType<DbContextOptions<HangfireContext>>(
+                    connection.GetFieldValue("_options")));
+        }
+
+        [Fact]
+        public void GetMonitoringApi_ReturnsCorrectResult()
+        {
+            var options = new DbContextOptions<HangfireContext>();
+            var instance = new EntityFrameworkCoreJobStorage(options);
+
+            var result = instance.GetMonitoringApi();
+
+            Assert.NotNull(result);
+            var api = Assert.IsType<EntityFrameworkCoreJobStorageMonitoringApi>(result);
+
+            Assert.Same(options,
+                Assert.IsType<DbContextOptions<HangfireContext>>(
+                    api.GetFieldValue("_options")));
+        }
+    }
+}
