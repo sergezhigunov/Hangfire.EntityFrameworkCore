218231b77cdb7d3e654fbdbdec2b95a9f759ead2
jack775544 <jack775544@gmail.com>
2021-04-03 10:26:08 +1000

    Create new migration to update model snapshot for sample project

--- samples/Hangfire.EntityFrameworkCore.AspNetCoreExternalDbContext/Migrations/20210403002419_StringFieldLength.Designer.cs
+++ samples/Hangfire.EntityFrameworkCore.AspNetCoreExternalDbContext/Migrations/20210403002419_StringFieldLength.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Hangfire.EntityFrameworkCore.AspNetCoreExternalDbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Hangfire.EntityFrameworkCore.AspNetCoreExternalDbContext.Migrations
 {
     [DbContext(typeof(SampleDbContext))]
-    partial class SampleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210403002419_StringFieldLength")]
+    partial class StringFieldLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
