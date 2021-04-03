2ac0123e1fa2e13c41839bbaf20a4f4a37eb216c
jack775544 <jack775544@gmail.com>
2021-04-03 10:26:08 +1000

    Create new migration to update model snapshot for sample project

---
+++ samples/Hangfire.EntityFrameworkCore.AspNetCoreExternalDbContext/Migrations/20210403002419_StringFieldLength.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hangfire.EntityFrameworkCore.AspNetCoreExternalDbContext.Migrations
+{
+    public partial class StringFieldLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
