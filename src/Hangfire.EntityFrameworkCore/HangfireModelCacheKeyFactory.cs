b573bfe9b97cd3147c3739eaebdcad014a0b39d3
Sergey Zhigunov <serge.zhigunov@outlook.com>
2021-11-30 15:44:53 +0500

    #15 Fix model cache key implementation to support EFCore 6

--- src/Hangfire.EntityFrameworkCore/HangfireModelCacheKeyFactory.cs
+++ src/Hangfire.EntityFrameworkCore/HangfireModelCacheKeyFactory.cs
@@ -5,6 +5,15 @@ namespace Hangfire.EntityFrameworkCore
 {
     internal class HangfireModelCacheKeyFactory : IModelCacheKeyFactory
     {
-        public object Create(DbContext context) => new HangfireModelCacheKey(context);
+#if !NET6_0_OR_GREATER
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822")]
+#endif
+        public object Create(DbContext context, bool designTime)
+            => context is HangfireContext hangfireContext
+                ? (context.GetType(), hangfireContext.Schema, designTime)
+                : (object)context.GetType();
+
+        public object Create(DbContext context)
+            => Create(context, false);
     }
 }
