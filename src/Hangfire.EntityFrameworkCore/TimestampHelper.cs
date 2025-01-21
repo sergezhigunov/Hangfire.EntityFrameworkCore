0e6b18cf324f25e401edccb5469727f8959c2917
Sergey Zhigunov <serge.zhigunov@mail.ru>
2025-01-21 11:49:29 +0500

    Fix TimestampHelper conditional compilation directives

--- src/Hangfire.EntityFrameworkCore/TimestampHelper.cs
+++ src/Hangfire.EntityFrameworkCore/TimestampHelper.cs
@@ -1,33 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Hangfire.EntityFrameworkCore;
 
-namespace Hangfire.EntityFrameworkCore
+internal static class TimestampHelper
 {
-    internal static class TimestampHelper
+    public static long GetTimestamp()
     {
-        public static long GetTimestamp()
-        {
-#if NETCOREAPP3_0
-            return Environment.TickCount64;
+#if NET8_0_OR_GREATER
+        return Environment.TickCount64;
 #else
-            return Environment.TickCount;
+        return Environment.TickCount;
 #endif
-        }
+    }
 
-        public static TimeSpan Elapsed(long timestamp)
-        {
-            long now = GetTimestamp();
-            return Elapsed(now, timestamp);
-        }
+    public static TimeSpan Elapsed(long timestamp)
+    {
+        long now = GetTimestamp();
+        return Elapsed(now, timestamp);
+    }
 
-        public static TimeSpan Elapsed(long now, long timestamp)
-        {
-#if NETCOREAPP3_0
-            return TimeSpan.FromMilliseconds(now - timestamp);
+    public static TimeSpan Elapsed(long now, long timestamp)
+    {
+#if NET8_0_OR_GREATER
+        return TimeSpan.FromMilliseconds(now - timestamp);
 #else
-            return TimeSpan.FromMilliseconds(unchecked((int)now - (int)timestamp));
+        return TimeSpan.FromMilliseconds(unchecked((int)now - (int)timestamp));
 #endif
-        }
     }
 }
